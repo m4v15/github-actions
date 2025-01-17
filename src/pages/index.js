0067b3eed1b44269f67888849b561fe0da5feda0
Matt Davis <matthewpeterdavis@gmail.com>
2025-01-17 10:35:24 +0000

    visual change

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            {process.env.SECRET}
+            WAHEYYYY
           </a>
         </div>
       </main>
